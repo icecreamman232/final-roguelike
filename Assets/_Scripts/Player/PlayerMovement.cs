11d073fb5fcad64ecbacd0b9a3895c4b7d6f65b8
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-08 02:13:36 +0700

    Add room visual + door + implement obstacle checking to player movement

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,15 @@ namespace SGGames.Scripts.Player
 {
     public class PlayerMovement : PlayerBehavior
     {
+        [Header("Obstacle Test")]
+        [SerializeField] private LayerMask m_obstacleLayerMask; 
+        [SerializeField] private Vector2 m_playerSize;
         [SerializeField] private Vector2 m_direction;
+        [Header("Speed")]
         [SerializeField] private float m_initialSpeed;
         [SerializeField] private float m_currentSpeed;
 
+        private readonly float m_raycastDistance = 0.2f;
         private bool m_canMove;
         private PlayerInputAction m_playerInput;
         
@@ -66,9 +71,22 @@ namespace SGGames.Scripts.Player
             m_direction = m_playerInput.Player.WASD.ReadValue<Vector2>();
         }
 
+        private bool CheckObstacle()
+        {
+            var hit = Physics2D.BoxCast(
+                transform.position,
+                m_playerSize,0,m_direction,m_raycastDistance,m_obstacleLayerMask);
+            return hit.collider != null;
+        }
+
         private void UpdateMovement()
         {
             if (!m_canMove) return;
+            if (CheckObstacle())
+            {
+                m_direction = Vector2.zero;
+            }
+            
             transform.Translate(m_direction * (m_currentSpeed * Time.deltaTime));
         }
     }
