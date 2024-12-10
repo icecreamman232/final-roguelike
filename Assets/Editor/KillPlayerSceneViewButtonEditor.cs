d7e0ba09e324f710de21827ac13ee28e6e9908de
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-10 21:27:05 +0700

    Test scene view button

---
+++ Assets/Editor/KillPlayerSceneViewButtonEditor.cs
@@ -0,0 +1,44 @@
+
+#if UNITY_EDITOR
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class KillPlayerSceneViewButtonEditor : Editor
+{
+    private static GUIStyle m_style = new GUIStyle();
+    
+    [MenuItem("SGGames/Show Scene Tools")]
+    public static void Enable()
+    {
+        SceneView.duringSceneGui += OnSceneGUIDrawn;
+        m_style = GUI.skin.label;
+    }
+
+    [MenuItem("SGGames/Hide Scene Tools")]
+    public static void Disable()
+    {
+        SceneView.duringSceneGui -= OnSceneGUIDrawn;
+    }
+
+    private void OnEnable()
+    {
+        m_style = GUI.skin.label;
+    }
+
+
+    private static void OnSceneGUIDrawn(SceneView sceneview)
+    {
+        Handles.BeginGUI();
+
+        m_style.alignment = TextAnchor.MiddleCenter;
+        if (GUI.Button(new Rect(0f, 0f, 200f, 50f), "Kill Player",m_style)) 
+        {
+            Debug.Log("Button Clicked");
+        }
+        Handles.EndGUI();
+    }
+}
+
+#endif
