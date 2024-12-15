4108da3dd76dbc92f531b13ea47de338ca707898
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-15 07:55:50 +0700

    Add script to import constant data from csv

---
+++ Assets/_Scripts/Data/ConstantData.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace SGGames.Scripts.Data
+{
+    [CreateAssetMenu(fileName = "ConstantData", menuName = "SGGames/Data/Constants")]
+    public class ConstantData : ScriptableObject
+    {
+        [SerializeField] private float m_maxAtkSpd;
+        [SerializeField] private float m_strToRegenerate;
+        [SerializeField] private float m_strToHealth;
+        [SerializeField] private float m_agiToAtkSpd;
+        [SerializeField] private float m_atkSpdToAtkRate;
+
+
+        public void SetData(float maxAtkSpd, float strToRegenerate, 
+            float strToHealth, float agiToAtkSpd, float atkSpdToAtkRate)
+        {
+            m_maxAtkSpd = maxAtkSpd;
+            m_strToRegenerate = strToRegenerate;
+            m_strToHealth = strToHealth;
+            m_agiToAtkSpd = agiToAtkSpd;
+            m_atkSpdToAtkRate = atkSpdToAtkRate;
+        }
+        
+        public float C_MAX_ATK_SPD => m_maxAtkSpd;
+        public float C_STR_TO_REGENERATE => m_strToRegenerate;
+        public float C_STR_TO_HEALTH => m_strToRegenerate;
+        public float C_AGI_TO_ATK_SPD => m_agiToAtkSpd;
+        public float C_ATK_SPD_TO_ATK_RATE => m_atkSpdToAtkRate;
+        
+    }
+}
+
---
+++ Assets/_Scripts/Tools/CSVReader.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+using SGGames.Scripts.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace SGGames.Scripts.Tools
+{
+    public class CSVReader 
+    {
+        [MenuItem("SGGames/Tools/Import Constant")]
+        public static void ImportConstantData()
+        {
+            var constantDataAsset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/_Data/constant-data.csv");
+            var lines = constantDataAsset.text.Split("\n");
+            
+            var max_atk_spd                        = float.Parse(lines[0].Split(',')[1]);
+            var str_to_regeneration_rate           = float.Parse(lines[1].Split(',')[1]);
+            var str_to_health                      = float.Parse(lines[2].Split(',')[1]);
+            var agi_to_atk_spd                     = float.Parse(lines[3].Split(',')[1]);
+            var atk_spd_to_atk_rate                = float.Parse(lines[4].Split(',')[1]);
+            
+            var data = AssetDatabase.LoadAssetAtPath<ConstantData>("Assets/_Data/ConstantData.asset");
+            data.SetData(max_atk_spd, str_to_regeneration_rate,str_to_health,agi_to_atk_spd,atk_spd_to_atk_rate);
+            EditorUtility.SetDirty(data);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
+
+#endif
