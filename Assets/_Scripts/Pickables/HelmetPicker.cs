c3f2d8514049659bf074f9c697c774cd7b6670d4
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-18 14:52:23 +0700

    Add helmet picker HUD

--- Assets/_Scripts/Pickables/HelmetPicker.cs
+++ Assets/_Scripts/Pickables/HelmetPicker.cs
@@ -1,16 +1,22 @@
 
+using SGGames.Scripts.UI;
+using UnityEngine;
+
 namespace SGGames.Scripts.Pickables
 {
     public class HelmetPicker : ItemPicker
     {
+        [SerializeField] private HelmetPickerHUD m_pickerHUD;
         protected override void ShowPrompt()
         {
             base.ShowPrompt();
+            m_pickerHUD.Show(m_itemData);
         }
 
         protected override void HidePrompt()
         {
             base.HidePrompt();
+            m_pickerHUD.Hide();
         }
     }
 }
---
+++ Assets/_Scripts/UI/HelmetPickerHUD.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SGGames.Scripts.UI
+{
+    public class HelmetPickerHUD : ItemPickerHUD
+    {
+        
+    }
+}
+
