b9194716a91a0904eb1bc2b7f133080ed6b22b3f
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-18 14:42:20 +0700

    Add boots picker hud

--- Assets/_Scripts/Pickables/BootsPicker.cs
+++ Assets/_Scripts/Pickables/BootsPicker.cs
@@ -1,15 +1,21 @@
+using SGGames.Scripts.UI;
+using UnityEngine;
+
 namespace SGGames.Scripts.Pickables
 {
     public class BoostPicker : ItemPicker
     {
+        [SerializeField] private BootsPickerHUD m_pickerHUD;
         protected override void ShowPrompt()
         {
             base.ShowPrompt();
+            m_pickerHUD.Show(m_itemData);
         }
 
         protected override void HidePrompt()
         {
             base.HidePrompt();
+            m_pickerHUD.Hide();
         }
     }
 }
---
+++ Assets/_Scripts/UI/BootsPickerHUD.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SGGames.Scripts.UI
+{
+    public class BootsPickerHUD : ItemPickerHUD
+    {
+        
+    }
+}
