90e0c953754e781f5e72ecc17af79819ef2e7724
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-19 23:27:53 +0700

    Fix upgrade card UI not being reset after played

--- Assets/_Scripts/UI/UpgradeAttribute/UpgradeAttributeCardUI.cs
+++ Assets/_Scripts/UI/UpgradeAttribute/UpgradeAttributeCardUI.cs
@@ -19,6 +19,7 @@ namespace SGGames.Scripts.UI
         [SerializeField] private Sprite m_legendaryBG;
         [SerializeField] private TextMeshProUGUI m_titleText;
         [SerializeField] private TextMeshProUGUI m_descText;
+        [SerializeField] private CanvasGroup m_canvasGroup;
         [SerializeField] private MMF_Player m_showFeedback;
         [SerializeField] private MMF_Player m_notSelectFeedback;
 
@@ -27,9 +28,24 @@ namespace SGGames.Scripts.UI
         public (UpgradeAttributeRate rate, AttributeType type) Reward => m_reward;
         
         public Action<int> OnSelectCard;
-        
+
+        private Vector2 m_initAnchorMin;
+        private Vector2 m_initAnchorMax;
+
+        protected override void Start()
+        {
+            base.Start();
+            m_initAnchorMin = ((RectTransform)transform).anchorMin;
+            m_initAnchorMax = ((RectTransform)transform).anchorMax;
+        }
+
         public void Show((UpgradeAttributeRate rate, AttributeType type) attributeReward)
         {
+            //Reset values
+            ((RectTransform)transform).anchorMin = m_initAnchorMin;
+            ((RectTransform)transform).anchorMax = m_initAnchorMax;
+            m_canvasGroup.alpha = 1;
+            
             m_showFeedback.PlayFeedbacks();
             m_reward = attributeReward;
             switch (attributeReward.rate)
