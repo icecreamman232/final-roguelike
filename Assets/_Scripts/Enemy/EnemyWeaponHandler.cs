303c0600d9b3e631b2e291b3671ac0a4b28ff798
Stephen Ho <stephen.ho.232@gmail.com>
2024-12-25 17:32:44 +0700

    Rotate weapon of enemy

--- Assets/_Scripts/Enemy/EnemyWeaponHandler.cs
+++ Assets/_Scripts/Enemy/EnemyWeaponHandler.cs
@@ -1,4 +1,3 @@
-
 using SGGames.Scripts.Attribute;
 using SGGames.Scripts.Weapons;
 using UnityEngine;
@@ -10,10 +9,14 @@ namespace SGGames.Scripts.Enemies
         [SerializeField] protected Weapon m_initialWeapon;
         [SerializeField][ReadOnly] protected Weapon m_currentWeapon;
         [SerializeField] protected Transform m_weaponAttachment;
+        [SerializeField] protected float m_offsetFromCenter;
+        [SerializeField] protected float m_offsetAngleWeapon;
+        protected EnemyController m_controller;
         
         protected override void Start()
         {
             base.Start();
+            m_controller = GetComponent<EnemyController>();
             Initialize();
         }
 
@@ -24,7 +27,21 @@ namespace SGGames.Scripts.Enemies
                 EquipWeapon(m_initialWeapon);
             }
         }
-        
+
+        protected override void Update()
+        {
+            if (!m_isAllow) return;
+
+            if (m_controller.CurrentBrain == null) return;
+            if (m_controller.CurrentBrain.Target == null) return;
+            
+            var directionToTarget = (m_controller.CurrentBrain.Target.transform.position - m_weaponAttachment.position).normalized * m_offsetFromCenter;
+            m_weaponAttachment.position = transform.position + directionToTarget;
+            m_weaponAttachment.rotation = Quaternion.AngleAxis(
+                Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg + m_offsetAngleWeapon, Vector3.forward);
+
+        }
+
         protected virtual void EquipWeapon(Weapon newWeapon)
         {
             m_currentWeapon = Instantiate(newWeapon, m_weaponAttachment);
