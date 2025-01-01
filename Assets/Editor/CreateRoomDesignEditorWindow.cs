7ff15e3d3d3da54d8eadbf77809c762626ea4832
Stephen Ho <stephen.ho.232@gmail.com>
2025-01-01 17:36:43 +0700

    Add room design tool

---
+++ Assets/Editor/CreateRoomDesignEditorWindow.cs
@@ -0,0 +1,67 @@
+
+#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SGGames.Scripts.Data;
+using SGGames.Scripts.Enemies;
+using SGGames.Scripts.Rooms;
+using UnityEditor;
+using UnityEngine;
+
+namespace SGGames.Scripts.EditorExtension
+{
+    public class CreateRoomDesignEditorWindow : EditorWindow
+    {
+        private RoomDifficultType m_difficultType;
+        private GameObject m_roomPrefab;
+        private int m_areaIndex;
+        private int m_postFixIndex;
+        
+        [MenuItem("SGGames/Create Room Tool")]
+        public static void OpenWindow()
+        {
+            var window = GetWindow<CreateRoomDesignEditorWindow>("Create Room Design Editor");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            m_difficultType = (RoomDifficultType)EditorGUILayout.EnumPopup("Difficulty",m_difficultType);
+            m_roomPrefab = (GameObject)EditorGUILayout.ObjectField("Room Prefab", m_roomPrefab, typeof(GameObject), false);
+            m_areaIndex = EditorGUILayout.IntField("Area Index", m_areaIndex);
+            m_postFixIndex = EditorGUILayout.IntField("Post-Fix Index", m_postFixIndex);
+            
+            EditorGUILayout.Space(30);
+            if (GUILayout.Button("Create Room Data"))
+            {
+                SaveRoomDesign();
+            }
+        }
+
+        private void SaveRoomDesign()
+        {
+            var enemyPrefabs = FindObjectsOfType(typeof(EnemyController), true);
+            var roomDataAsset = ScriptableObject.CreateInstance<RoomData>();
+            roomDataAsset.RoomPrefab = m_roomPrefab;
+            var enemyCount = enemyPrefabs.Length;
+            roomDataAsset.EnemyList = new EnemySpawnInfo[enemyCount];
+            for (int i = 0; i < enemyCount; i++)
+            {
+                var prefabPath = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(enemyPrefabs[i]);
+                
+                var asset = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                
+                roomDataAsset.EnemyList[i] = new EnemySpawnInfo
+                {
+                    EnemyPrefab = asset,
+                    SpawnPosition = asset.transform.position
+                };
+            }
+            AssetDatabase.CreateAsset(roomDataAsset, $"Assets/_Data/RoomData/Area-{m_areaIndex}/{m_roomPrefab.name}_{m_difficultType}_{m_postFixIndex}.asset");
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
+
+#endif
--- Assets/_Scripts/Data/RoomData.cs
+++ Assets/_Scripts/Data/RoomData.cs
@@ -3,14 +3,31 @@ using UnityEngine;
 
 namespace SGGames.Scripts.Data
 {
+    public enum RoomDifficultType
+    {
+        Easy,
+        Hard,
+        Challenge
+    }
+    
     [CreateAssetMenu(fileName = "RoomData", menuName = "SGGames/Data/Room Data")]
     public class RoomData : ScriptableObject
     {
         [SerializeField] private GameObject m_roomPrefab;
         [SerializeField] private EnemySpawnInfo[] m_enemyList;
         
-        public GameObject RoomPrefab => m_roomPrefab;
-        public EnemySpawnInfo[] EnemyList => m_enemyList;
+        public GameObject RoomPrefab
+        {
+            get => m_roomPrefab;
+
+            set => m_roomPrefab = value;
+        }
+
+        public EnemySpawnInfo[] EnemyList
+        {
+            get => m_enemyList;
+            set => m_enemyList = value;
+        }
     }
 
     [Serializable]
