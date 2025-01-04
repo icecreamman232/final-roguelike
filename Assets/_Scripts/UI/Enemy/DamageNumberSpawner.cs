2add0c2e094b2107d9bb2c4b6397b93f89f618f7
Stephen Ho <stephen.ho.232@gmail.com>
2025-01-04 16:02:37 +0700

    Make damage number spawn randomly around entity

--- Assets/_Scripts/UI/Enemy/DamageNumberSpawner.cs
+++ Assets/_Scripts/UI/Enemy/DamageNumberSpawner.cs
@@ -11,6 +11,7 @@ namespace SGGames.Scripts.UI
         [SerializeField] private ObjectPooler m_damageNumberPooler;
         [SerializeField] private Color m_normalColor;
         [SerializeField] private Color m_criticalColor;
+        [SerializeField] private float m_showRadius;
 
         private readonly float m_criticalHitTextScale = 1.3f;
         
@@ -29,6 +30,7 @@ namespace SGGames.Scripts.UI
         {
             var dmgNumberObj = m_damageNumberPooler.GetPooledGameObject();
             dmgNumberObj.transform.localScale = isCritical ? Vector3.one * m_criticalHitTextScale : Vector3.one;
+            dmgNumberObj.transform.position = transform.position + (Vector3)Random.insideUnitCircle * m_showRadius;
             var damageText = dmgNumberObj.GetComponentInChildren<TextMeshPro>();
             damageText.color = isCritical ? m_criticalColor : m_normalColor;
             damageText.text = isCritical ? $"{damage.ToString()} !" :damage.ToString();
