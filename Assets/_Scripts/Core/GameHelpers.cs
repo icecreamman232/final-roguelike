edca74cf9775671024ecb845f688963eed8a1871
Stephen Ho <stephen.ho.232@gmail.com>
2025-01-06 02:45:03 +0700

    Add some extension method for vector2 and vector3

--- Assets/_Scripts/Core/GameHelpers.cs
+++ Assets/_Scripts/Core/GameHelpers.cs
@@ -37,5 +37,29 @@ namespace SGGames.Scripts.Core
             return Guid.NewGuid().ToString();
         }
         #endregion
+        
+        #region Vector
+
+        public static Vector2 With(this Vector2 vector, float? x = null, float? y = null)
+        {
+            return new Vector2(x ?? vector.x, y ?? vector.y);
+        }
+
+        public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(x ?? vector.x, y ?? vector.y, z ?? vector.z);
+        }
+
+        public static Vector2 Add(this Vector2 vector, float? x = null, float? y = null)
+        {
+            return new Vector2(vector.x + (x ?? 0), vector.y + (y ?? 0));
+        }
+        
+        public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(vector.x + (x ?? 0), vector.y + (y ?? 0), vector.z + (z ?? 0));
+        }
+        
+        #endregion
     }
 }
